e323969ef5d828be28c138fea2808e41501e4ff6
YwaCernok <93765179+YwaCernok@users.noreply.github.com>
2021-11-17 16:40:02 +0400

    Finished exo1

---
+++ Exo1/assets/js/script.js
@@ -0,0 +1,16 @@
+/*
+$(function(){
+    $ ('body').click(function(){
+        $('.text').hide();
+    })
+
+
+});
+
+*/
+
+$ (function() {
+    $ ('.btn').click(function(){
+        $('.text').hide();
+    })
+});
